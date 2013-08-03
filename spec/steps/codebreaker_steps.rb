f06aa0d70761cfc156ac8b93d884904a1a2dfc62
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2013-08-03 10:43:46 +0900

    finished chapter 8

--- spec/steps/codebreaker_steps.rb
+++ spec/steps/codebreaker_steps.rb
@@ -6,11 +6,25 @@ end
 step "I start a new game" do 
   @output = Output.new
   @game = Codebreaker::Game.new(@output)
-  @game.start
+  @game.start('1234')
 end
 
 step "I should see :message" do |message|
   @output.messages.should include(message)
+end
+
+step "the secret code is :code" do |code|
+  @output = Output.new
+  @game = Codebreaker::Game.new(@output)
+  @game.start(code)
+end
+
+step "I guess :guess" do |guess|
+  @game.guess(guess)
+end
+
+step "the mark should be :mark" do |mark|
+  @output.messages.should include(mark)
 end
 
 class Output
